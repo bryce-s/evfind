700dc83633f8da3d3d3946a44bc74c45698cc98e
Bryce Smith <bryces@umich.edu>
2019-10-23 10:28:40 -0400

    parses search results

--- evfind/EverythingSearchAdapter.cpp
+++ evfind/EverythingSearchAdapter.cpp
@@ -7,7 +7,11 @@ bool EverythingSearchAdapter::everythingServiceIsRunning()
 
 void EverythingSearchAdapter::searchTerm(const std::string& term)
 {
-	Everything_SetSearch(LPCWSTR("Bryce"));
+	// long pointer to constant wide string--prefix with L
+	// to make string wide. Is 2 bytes per char.
+	LPCWSTR query = L"Bryce";
+
+	Everything_SetSearch(LPCWSTR(query));
 	Everything_Query(true);
 	for (auto i = 0; i < Everything_GetNumResults(); i++) {
 		std::cout << Everything_GetResultFileName(i) << std::endl;
