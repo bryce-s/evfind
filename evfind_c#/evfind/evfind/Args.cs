5f1b7163e42e15e26340752ffe3c43d63b74485d
bryces <bryces@umich.edu>
2020-06-15 19:21:04 -0400

    change to c#

---
+++ evfind_c#/evfind/evfind/Args.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace evfind
+{
+    
+    class Args
+    {
+        public class Options
+        { 
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose.")]
+            public bool Verbose { get; set; }
+        }
+        public Args(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
+            {
+                if (o.Verbose)
+                {
+                    Console.WriteLine("verbose enabled");
+                }
+            });
+
+        }
+    }
+}
